15848480024839e1a628dbe5cd4b2e019a9ff9c5
Teddys09 <ted3009@hotmail.fr>
2022-10-16 15:00:11 +0200

    Add case if cart empty

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Card from '../../components/Card/Card';
 import { useNavigate } from 'react-router-dom';
 import ArrowLeft from '../../img/arrow-left.svg';
+import { Link } from 'react-router-dom';
 
 const ShoppingCart = () => {
   const cart = useSelector((state) => state.store.cart);
@@ -11,21 +12,30 @@ const ShoppingCart = () => {
   const totalPrice = cart.reduce((acc, item) => acc + item.price, 0).toFixed(2);
   console.log(totalPrice);
   const navigate = useNavigate();
-  return (
-    <div className="cart-container">
-      <h1>Shopping Cart</h1>
-      <div className="card-price-container">
-        <Card products={cart} />
-        <div className="totalPrice">
-          <h2>Total Price: {totalPrice}€</h2>
-          <div className="purchase-button">Purchase</div>
+  if (cart.length === 0) {
+    return (
+      <div className="cart-empty">
+        <h2>Your cart is empty</h2>
+        <Link to="/"> Go back to shop</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="cart-container">
+        <h1>Shopping Cart</h1>
+        <div className="card-price-container">
+          <Card products={cart} />
+          <div className="totalPrice">
+            <h2>Total Price: {totalPrice}€</h2>
+            <div className="purchase-button">Purchase</div>
+          </div>
+        </div>
+        <div className="arrow-left">
+          <img src={ArrowLeft} alt="arrow left" onClick={() => navigate('/')} />
         </div>
       </div>
-      <div className="arrow-left">
-        <img src={ArrowLeft} alt="arrow left" onClick={() => navigate('/')} />
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default ShoppingCart;
